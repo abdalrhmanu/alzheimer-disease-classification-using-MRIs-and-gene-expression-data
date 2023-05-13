98748792546e06f526ba4215b6b1ec8a9d85ccec
ABDELRAHMAN HABIB <abdalrhmanusama@hotmail.com>
2023-05-13 09:44:25 +0200

    final ADCTL classification using SVM

--- submission.R
+++ submission.R
@@ -1,4 +1,6 @@
-# install.packages("usdm")
+list.packages <- c("caret", "glmnet", "rpart", "randomForest", "e1071", "ggplot2", "usdm")
+new.packages <- list.packages[!(list.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
 
 library(caret)
 library(glmnet)
@@ -18,87 +20,69 @@ ADMCItest_dir <- "./data/test/ADMCItest.csv"
 MCICTLtest_dir <- "./data/test/MCICTLtest.csv"
 
 # Reading data
-train_data <- read.csv(ADCTLtrain_dir, header = TRUE)
-test_data <- read.csv(ADCTLtest_dir, header = TRUE)
+train_data_ADCTL <- read.csv(ADCTLtrain_dir, header = TRUE)
+test_data_ADCTL <- read.csv(ADCTLtest_dir, header = TRUE)
 
 # dropping the id col
-train_data <- train_data[, -1] 
-test_data <- test_data[, -1]
+train_data_ADCTL <- train_data_ADCTL[, -1] 
+test_data_ADCTL <- test_data_ADCTL[, -1]
 
 # encoding label col in training data
-train_data$Label <- ifelse(train_data$Label == "AD", 1, 0)
+train_data_ADCTL$Label <- ifelse(train_data_ADCTL$Label == "AD", 1, 0)
 
 # Checking if the dataset has the non-numeric columns
-non_numeric_cols <- names(train_data)[!sapply(train_data, is.numeric)]
+non_numeric_cols <- names(train_data_ADCTL)[!sapply(train_data_ADCTL, is.numeric)]
 cat(non_numeric_cols)
 
 # Compute the correlation matrix between the variables in the dataset
-cor_matrix <- cor(train_data)
+cor_matrix <- cor(train_data_ADCTL)
 
 # Identify pairs of features with a correlation coefficient above 0.7
 highly_correlated <- findCorrelation(cor_matrix, cutoff = 0.7)
 
 # Remove the problematic features from the dataset
-train_data_clean <- train_data[,-highly_correlated]
+train_data_ADCTL_clean <- train_data_ADCTL[,-highly_correlated]
 
 # Convert the labels to a factor
-train_data_clean$Label <- as.factor(train_data_clean$Label)
+train_data_ADCTL_clean$Label <- as.factor(train_data_ADCTL_clean$Label)
+
 
 # Split the data into training, validation, and test sets
 set.seed(123)
-trainIndex <- createDataPartition(train_data_clean$Label, p = 0.7, list = FALSE)
-train <- train_data_clean[trainIndex,]
-valid_test <- train_data_clean[-trainIndex,]
+trainIndex <- createDataPartition(train_data_ADCTL_clean$Label, p = 0.7, list = FALSE)
+train <- train_data_ADCTL_clean[trainIndex,]
+valid_test <- train_data_ADCTL_clean[-trainIndex,]
 
 validIndex <- createDataPartition(valid_test$Label, p = 0.5, list = FALSE)
 validation <- valid_test[validIndex,]
 test <- valid_test[-validIndex,]
 
-##### Lasso
-# # Set up the Lasso regression model
-# train$Label <- as.numeric(train$Label)
-# lasso_model <- cv.glmnet(as.matrix(train[,1:ncol(train)-1]), train$Label, type.measure = "class", nfolds = 10)
-# 
-# # Extract the selected features for each dataset
-# train_selected <- predict(lasso_model, newx = as.matrix(train[,1:ncol(train)-1]), s = "lambda.min")
-# validation_selected <- predict(lasso_model, newx = as.matrix(validation[,1:ncol(validation)-1]), s = "lambda.min")
-# test_selected <- predict(lasso_model, newx = as.matrix(test[,1:ncol(test)-1]), s = "lambda.min")
+# Perform Recursive Feature Elimination (RFE) using caret
+ctrl <- rfeControl(functions = rfFuncs, method = "cv", number = 10)  # Use random forest as the underlying model
+result <- rfe(train[, -ncol(train)], train$Label, sizes = c(1:ncol(train)-1), rfeControl = ctrl)
 
-##### RFE
-# # Define the control parameters for the RFE algorithm
-# control <- rfeControl(functions = rfFuncs,
-#                       method = "cv",
-#                       number = 10)
-# 
-# # Define the model to be used by RFE
-# model <- train(Label ~ ., data = train, method = "rf")
-# 
-# # Use RFE to perform feature selection
-# results <- rfe(train[, -1], train$Label, sizes = c(1:10), rfeControl = control, method = "rf")
-# 
-# # Print the results
-# print(results)
-# 
-# # Plot the results
-# plot(results)
+# Get the selected features
+selected_features <- result$optVariables
 
-##### SVM
+# Subset the training, validation, and test sets with the selected features
+train_features <- train[, selected_features]
+validation_features <- validation[, selected_features]
+test_features <- test[, selected_features]
 
 # Define the features and labels
-train_features <- train[,2:ncol(train)-1]
-train_labels <- train[,ncol(train)]
-validation_features <- validation[,2:ncol(validation)-1]
-validation_labels <- validation[,ncol(validation)]
-test_features <- test[,2:ncol(test)-1]
-test_labels <- test[,ncol(test)]
+train_labels <- train$Label
+validation_labels <- validation$Label
+test_labels <- test$Label
 
 # Convert labels to factor with levels "AD" and "CTL"
 train_labels_factor <- factor(train_labels, levels = c(1, 0))
 validation_labels_factor <- factor(validation_labels, levels = c(1, 0))
 test_labels_factor <- factor(test_labels, levels = c(1, 0))
 
+######### SVM Model ######### 
 # Train an SVM classifier using the training set
-svm_model <- svm(train_features, train_labels_factor)
+svm_model <- svm(train_features, train_labels_factor,
+                 trControl = ctrl)
 
 # Tune the SVM hyperparameters using the validation set
 tuned_svm_model <- tune.svm(train_features, train_labels_factor,
@@ -117,7 +101,8 @@ confusionMatrix(test_pred_factor, test_labels_factor)
 
 # Predicting on test data
 # Preprocess the test data, MUST be same as training preprocessing!!
-test_features_prediction <- test_data[,-highly_correlated]
+test_features_prediction <- test_data_ADCTL[,-highly_correlated]
+test_features_prediction <- test_features_prediction[selected_features]
 
 # Use the trained SVM model to predict labels for the test data
 test_pred <- predict(tuned_svm_model$best.model, test_features_prediction)
