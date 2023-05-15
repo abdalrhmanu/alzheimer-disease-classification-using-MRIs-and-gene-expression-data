d7b1a76af071809b51315bf172dc153374e94a15
ABDELRAHMAN HABIB <abdalrhmanusama@hotmail.com>
2023-05-15 20:26:22 +0200

    finalizing ADCTL submission

--- submission.R
+++ submission.R
@@ -1,5 +1,5 @@
 ####  0.0 Installing necessary packages
-list.packages <- c("caret", "glmnet", "rpart", "randomForest", "e1071", "ggplot2", "usdm", "Boruta", "gbm")
+list.packages <- c("caret", "glmnet", "rpart", "randomForest", "e1071", "ggplot2", "usdm", "Boruta", "gbm", "pROC")
 new.packages <- list.packages[!(list.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
@@ -12,6 +12,8 @@ library(ggplot2)
 library(usdm)
 library(Boruta)
 library(gbm)
+library(pROC)  # Load the pROC package for AUC calculation
+
 
 
 ####  0.1 Defining directories
@@ -23,42 +25,123 @@ ADCTL_test_dir <- "./data/test/ADCTLtest.csv"
 ADMCI_test_dir <- "./data/test/ADMCItest.csv"
 MCICTL_test_dir <- "./data/test/MCICTLtest.csv"
 
-####  0.2 Reading data
-train_data_ADCTL <- read.csv(ADCTL_train_dir, header = TRUE)
-test_data_ADCTL <- read.csv(ADCTL_test_dir, header = TRUE)
+#### Defining functions
+calculate_metrics <- function(predicted_labels, actual_labels) {
+  library(pROC)  # Load the pROC package for AUC calculation
+  
+  # Convert factor labels to numeric
+  predicted_numeric <- as.numeric(predicted_labels) - 1
+  actual_numeric <- as.numeric(actual_labels) - 1
+  
+  # Calculate AUC
+  roc_obj <- roc(actual_numeric, predicted_numeric)
+  auc <- auc(roc_obj)
+  
+  # Calculate MCC
+  TP <- sum(predicted_labels == "AD" & actual_labels == "AD")
+  TN <- sum(predicted_labels == "CTL" & actual_labels == "CTL")
+  FP <- sum(predicted_labels == "AD" & actual_labels == "CTL")
+  FN <- sum(predicted_labels == "CTL" & actual_labels == "AD")
+  
+  # Check for division by zero
+  if (TP + FP == 0 || TP + FN == 0 || TN + FP == 0 || TN + FN == 0) {
+    mcc <- NaN
+    print("HERE")
+  } else {
+    mcc <- (TP * TN - FP * FN) / sqrt((TP + FP) * (TP + FN) * (TN + FP) * (TN + FN))
+  }
+  
+  # Calculate precision
+  precision <- TP / (TP + FP)
+  
+  # Calculate recall
+  recall <- TP / (TP + FN)
+  
+  # Calculate F1 score
+  f1_score <- 2 * (precision * recall) / (precision + recall)
+  
+  # Calculate sensitivity
+  sensitivity <- TP / (TP + FN)
+  
+  # Calculate specificity
+  specificity <- TN / (TN + FP)
+  
+  # Calculate Balanced Accuracy
+  ba <- (sensitivity + specificity) / 2
+  
+  
+  # Print the calculated metrics
+  print(paste("AUC:", auc))
+  print(paste("MCC:", mcc))
+  print(paste("Precision:", precision))
+  print(paste("F1 Score:", f1_score))
+  print(paste("Balanced Accuracy:", ba))
+  
+  
+  # Return the calculated metrics
+  return(list(AUC = auc, MCC = mcc, Precision = precision, Recall = recall, F1_Score = f1_score, Balanced_Accuracy = ba))
+}
+
 
 #############################################
 #                   ADCTL
 ######### DATASET 1 EXPERIMENTAL ############
 #############################################
-####  3.0 Pre-processing the data
-#     3.1 Dropping the id col in all datasets
+
+####  1.0 Reading data
+train_data_ADCTL <- read.csv(ADCTL_train_dir, header = TRUE)
+test_data_ADCTL <- read.csv(ADCTL_test_dir, header = TRUE)
+
+####  2.0 Pre-processing the data
+#     2.1 Dropping the id col in all datasets
 train_data_ADCTL <- train_data_ADCTL[, -1] 
 test_data_ADCTL <- test_data_ADCTL[, -1]
 
-#     3.2 Encoding label col in training data
+dim(train_data_ADCTL)
+dim(test_data_ADCTL)
+
+#     2.2 Encoding label col in training data
 train_data_ADCTL$Label <- ifelse(train_data_ADCTL$Label == "AD", 1, 0)
 
-#     3.3 Checking if the data set has the non-numeric columns
+#     2.3 Checking if the data set has the non-numeric columns
 non_numeric_cols <- names(train_data_ADCTL)[!sapply(train_data_ADCTL, is.numeric)]
 print(non_numeric_cols)
 
-#     4.0 Feature selection using correlation on ADCTL
-#     4.1 Compute the correlation matrix between the variables in the dataset
+#     3.0 Feature selection using correlation on ADCTL
+#     3.1 Compute the correlation matrix between the variables in the dataset
 cor_matrix <- cor(train_data_ADCTL)
 
-#     4.2 Identify pairs of features with a correlation coefficient above 0.7
+#     3.2 Identify pairs of features with a correlation coefficient above 0.7
 highly_correlated <- findCorrelation(cor_matrix, cutoff = 0.7)
 
-#     4.3 Remove the problematic features from the dataset
+#     3.3 Remove the problematic features from the dataset
 train_data_ADCTL_clean <- train_data_ADCTL[,-highly_correlated]
+test_data_ADCTL_clean <- test_data_ADCTL[,-highly_correlated]
 
-#     5.0 Convert the labels to a factor
+#     3.4 Convert the labels to a factor
 train_data_ADCTL_clean$Label <- as.factor(train_data_ADCTL_clean$Label)
 
-################################################################################
-####  6.0 Data Splitting
-#     6.1 Split the data into training, validation, and test sets
+#     4.0 Recursive Feature Elimination
+#     4.1 Perform Recursive Feature Elimination (RFE) using caret
+ctrl <- rfeControl(functions = rfFuncs, method = "cv", number = 10)  # Use random forest as the underlying model
+result <- rfe(train_data_ADCTL_clean[, -ncol(train_data_ADCTL_clean)], train_data_ADCTL_clean$Label, sizes = c(1:ncol(train_data_ADCTL_clean)-1), rfeControl = ctrl)
+
+#     4.2 Extract the top 5 variables from the RFE results
+selected_features <- result$optVariables[1:5]
+
+#     4.3 Subset the training and test sets with the selected features
+train_data_ADCTL_clean <- train_data_ADCTL_clean[, c(selected_features, "Label")]
+test_data_ADCTL_clean <- test_data_ADCTL_clean[, selected_features]
+
+dim(train_data_ADCTL_clean)
+dim(test_data_ADCTL_clean)
+
+#     4.4 Convert the labels to a factor
+train_data_ADCTL_clean$Label <- as.factor(train_data_ADCTL_clean$Label)
+
+
+#     5.0 Data Splitting
+#     5.1 Split the data into training, validation, and test sets
 set.seed(123)
 trainIndex <- createDataPartition(train_data_ADCTL_clean$Label, 
                                   p = 0.7, list = FALSE)
@@ -69,63 +152,59 @@ validIndex <- createDataPartition(valid_test$Label, p = 0.5, list = FALSE)
 validation <- valid_test[validIndex,]
 test <- valid_test[-validIndex,]
 
-####  6.0 Recursive Feature Elimination
-#     6.1 Perform Recursive Feature Elimination (RFE) using caret
-ctrl <- rfeControl(functions = rfFuncs, method = "cv", number = 10)  # Use random forest as the underlying model
-result <- rfe(train[, -ncol(train)], train$Label, sizes = c(1:ncol(train)-1), rfeControl = ctrl)
+#     5.2 Convert the labels to factors for all splits
+train$Label <- as.factor(train$Label)
+validation$Label <- as.factor(validation$Label)
+test$Label <- as.factor(test$Label)
 
-#     6.2 Get the selected features
-selected_features <- result$optVariables
+#     6.0 Training dataset ADCTL
+#     6.1 Train an SVM classifier using the training set
+svm_model <- svm(train[, -ncol(train)], train$Label,
+                 trControl = ctrl, probability = TRUE)
 
-#     6.3 Subset the training, validation, and test sets with the selected features
-train_features <- train[, selected_features]
-validation_features <- validation[, selected_features]
-test_features <- test[, selected_features]
-
-#     6.4 Define the features and labels
-train_labels <- train$Label
-validation_labels <- validation$Label
-test_labels <- test$Label
-
-#     6.5 Convert labels to factor with levels "AD" and "CTL"
-train_labels_factor <- factor(train_labels, levels = c(1, 0))
-validation_labels_factor <- factor(validation_labels, levels = c(1, 0))
-test_labels_factor <- factor(test_labels, levels = c(1, 0))
-
-####  7.0 Training dataset ADCTL Using SVM
-#     7.1 Train an SVM classifier using the training set
-svm_model <- svm(train_features, train_labels_factor,
-                 trControl = ctrl)
-
-#     7.2 Tune the SVM hyper-parameters using the validation set
-tuned_svm_model <- tune.svm(train_features, train_labels_factor,
+####  6.2 Tune the SVM hyper-parameters using the validation set
+tuned_svm_model <- tune.svm(train[, -ncol(train)], train$Label,
                             gamma = 10^(-6:-1), cost = 10^(0:3),
                             kernel = "radial", tunecontrol = tune.control(sampling = "cross"),
-                            validation.x = validation_features, validation.y = validation_labels_factor)
+                            validation.x = validation[, -ncol(validation)], validation.y = validation$Label,
+                            probability = TRUE)
 
-#     7.3 Use the tuned model to predict labels for the test data
-test_pred <- predict(tuned_svm_model$best.model, test_features)
+#     6.3 Extract the best model from the tuning results
+best_svm_model <- tuned_svm_model$best.model
 
-#     7.4 Convert test_pred to factor with levels "AD" and "CTL"
-test_pred_factor <- factor(test_pred, levels = c(1, 0))
+#     7.0 Evaluation on test set
+#     7.1 Use the best model to predict labels for the test set of the training data
+test_pred <- predict(best_svm_model, test[, -ncol(test)], probability = TRUE)
 
-#     7.5 Evaluate the model's performance on the test data
+#     7.2 Convert test_pred to factor with levels "AD" and "CTL"
+test_pred_factor <- factor(test_pred, levels = c(1, 0), labels = c("AD", "CTL"))
+
+#     7.3 Get the predicted probabilities for each class
+predicted_probabilities <- attr(test_pred, "probabilities")
+
+print(data.frame(Label = test_pred_factor,
+                 AD_Probability = predicted_probabilities[, 1],
+                 CTL_Probability = predicted_probabilities[, 0]))
+
+#     7.4 Convert the labels to factors with levels "AD" and "CTL"
+test_labels_factor <- factor(test$Label, levels = c(1, 0), labels = c("AD", "CTL"))
 confusionMatrix(test_pred_factor, test_labels_factor)
+
+#     7.5 Evaluate the model's performance on the test set
+calculate_metrics(test_pred_factor, test_labels_factor)
 
 ####  8.0 Predicting on  test data
 #     8.1 Preprocess the test data, MUST be same as training preprocessing!!
-test_features_prediction <- test_data_ADCTL[,-highly_correlated]
-test_features_prediction <- test_features_prediction[selected_features]
+test_features_prediction <- test_data_ADCTL_clean
 
 #     8.2 Use the trained SVM model to predict labels for the test data
-test_pred <- predict(tuned_svm_model$best.model, test_features_prediction)
+test_pred <- predict(tuned_svm_model$best.model, test_features_prediction, probability = TRUE)
 
 #     8.3 Convert test_pred to factor with levels "AD" and "CTL"
-test_pred_factor <- factor(test_pred, levels = c(1, 0))
+test_pred_factor <- factor(test_pred, levels = c(1, 0), labels = c("AD", "CTL"))
 
 #     8.4 Print the predicted labels for the test data
 print(test_pred_factor)
-
 
 
 #############################################
@@ -226,6 +305,14 @@ test_labels_factor <- as.factor(test_labels)
 # Create the confusion matrix
 confusionMatrix(test_predictions_factor, test_labels_factor)
 
+#     7.4 Convert test_pred to factor with levels "AD" and "CTL"
+test_labels_factor <- factor(test_labels_factor, levels = c(0, 1), labels = c("CTL", "AD"))
+test_pred_factor <- factor(test_predictions_factor, levels = c(0, 1), labels = c("CTL", "AD"))
+
+#     7.5 Evaluate the model's performance on the test data
+calculate_metrics(test_pred_factor, test_labels_factor)
+
+
 # Select the features from the test data using the same selected features
 test_data_selected <- test_data_ADMCI[, selected_features]
 
@@ -314,6 +401,14 @@ validation_labels <- factor(validation_labels, levels = c(0, 1))
 # Evaluate the model on the validation data
 validation_results <- confusionMatrix(validation_predictions, validation_labels)
 print(validation_results)
+
+#     7.4 Convert test_pred to factor with levels "AD" and "CTL"
+test_labels_factor <- factor(validation_predictions, levels = c(0, 1), labels = c("CTL", "AD"))
+test_pred_factor <- factor(validation_labels, levels = c(0, 1), labels = c("CTL", "AD"))
+
+#     7.5 Evaluate the model's performance on the test data
+calculate_metrics(test_pred_factor, test_labels_factor)
+
 
 # Perform the same feature selection using the Lasso model
 selected_test_features <- as.matrix(test_data_MCICTL)[, which(coef(lasso_model, s = "lambda.min") != 0)]
